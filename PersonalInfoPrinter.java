661016bd22b8c6b354226452ed8442671698494e
vulcantoaster <vulcantoaster@gmail.com>
2025-12-14 18:26:29 -0500

    Add files via upload

---
+++ PersonalInfoPrinter.java
@@ -0,0 +1,19 @@
+public class PersonalInfoPrinter {
+    public static void main(String[] args) {
+        
+        // 1. Define and Assign Fictional Values to String Variables
+        String firstName = "Alex";
+        String lastName = "Rivera";
+        String streetAddress = "123 Main St";
+        String city = "Anytown";
+        String zipCode = "90210";
+        
+        // 2. Output Each Piece of Information on a New Line
+        // The println() method automatically adds a newline character after the output.
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(streetAddress);
+        System.out.println(city);
+        System.out.println(zipCode);
+    }
+}
